ae2e1370f2a8db395ab2f0e8a1ff1a93f1524a2c
shrobin13 <145149707+shrobin13@users.noreply.github.com>
2026-05-23 15:42:46 +0600

    fixed update Permission for users

--- src/modules/issues/issues.route.ts
+++ src/modules/issues/issues.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { issuesController } from "./issues.controller";
 import auth from "../../middleware/auth";
 import { UserRole } from "../../enums-types/enums";
+import updatePermission from "../../middleware/update";
 
 const router = Router();
 
@@ -15,11 +16,7 @@ router.get("/", issuesController.getAllIssues);
 
 router.get("/:id", issuesController.getIssueById);
 
-router.patch(
-  "/:id",
-  auth(UserRole.maintainer),
-  issuesController.updateIssueById,
-);
+router.patch("/:id", updatePermission, issuesController.updateIssueById);
 
 router.delete(
   "/:id",
